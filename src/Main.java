a63ad922bb76c662c0ef45a5664e331f2de761eb
AlelsanderKupriyanov <kupriyanov.a.a@yandex.ru>
2025-05-06 20:53:07 +0300

    V1.2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] array = {45, 78, -15, 12, 34, 6, 90, 7, -2, -45};
+        int[] array = {3, 4, -15, -14, 34, 6, 90, 7, -2, -45};
         int i, max = array[0], min = array[0];
 
         //Задание 1.
@@ -26,12 +26,11 @@ public class Main {
         System.out.println();
 
         //Задание 3.
-        int l = 0, s, t = 0, a, d = 1;
+        int l = 0, t = 0, a, d = 1;
         for (i = 1; i < array.length; i++) {
             if (array[i] >= l) l = array[i];
         }
         System.out.println("Самое длинное число = " + l);
-        s = l;
         while (t == 0) {
             d = d * 10;
             for (i = 0; i < array.length; i++) {
@@ -42,5 +41,22 @@ public class Main {
                 }
             }
         }
+        //Задание 4.
+        //Сортируем по возрастанию числа в массиве
+        int z;
+        for (i = 0; i < array.length; i++) {
+            for (j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    z = array[i];
+                    array[i] = array[j];
+                    array[j] = z;
+                }
+            }
+        }
+        //Ищем числа идущие по порядку
+        for (i = 0; i < array.length - 1; i++) {
+            if (array[i] == (array[i + 1] - 1)) System.out.println(array[i] + " " + array[i + 1]);
+        }
+        //Задание 5.
     }
 }
